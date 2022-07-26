459ac3bac064ed3bdeba73083b985e400aeaa960
jack-leyland <jackjleyland@gmail.com>
2022-07-26 14:54:36 +0100

    first build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import './App.css';
 import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 const Title = (props) => {
+  const [active, setActive] = useState(false)
+
+  const selectHandler = (id) => {
+    props.handler(id)
+    setActive(!active)
+  }
+
   return(
     <div className="font-albert-600 text-gray-800 font-medium h-[20%] w-full m-auto text-5xl pb-[20px]">
       Node<span className="text-green-600">Watts</span> <span className='text-3xl mr-[20px]'>Visualizer</span>
-      <span className="text-lg text-gray-700">Profile: {props.name}</span>
+      <span className="text-lg text-gray-700 whitespace-pre">Profile: {props.name}{"   "}</span>
+      <Button title="Select Profile" active={true} buttonHandler={() => {setActive(!active)}}/>
+      {props.options && active && <ProfileDropDown options={props.options} selectHandler={selectHandler}/>}
+    </div>
+  )
+}
+
+const ProfileDropDown = (props) => {
+  const [options, setOptions] = useState(null)
+
+  useEffect(()=> {
+    props.options.forEach((item)=>{
+      item["key"] = uuidv4()
+    })
+    setOptions(props.options)
+  }, [props.options])
+
+  const clickHandler = (key) => {
+    var id;
+    options.forEach((item) => {
+      if (item["key"] === key){ id = item["_id"]}
+    })
+    props.selectHandler(id)
+  }
+
+  return(
+    <div className="w-[600px] h-[400px] rounded-md bg-gray-200 absolute left-[553px] mt-[5px]">
+      {options && options.map((data) => {
+        return (<div 
+                  className="text-sm p-2 hover:bg-green-200 rounded-md cursor-pointer"
+                  key={data.key}
+                  onClick={()=>clickHandler(data.key)}
+                >
+                  {data.name}
+                </div>)
+      })}
     </div>
   )
 }
@@ -34,9 +77,9 @@ const NavBar = (props) => {
 
 const Button = (props) => {
   if (props.active){
-    var styling = "text-gray-800 font-ubuntu rounded-md w-[150px] h-[30px] mr-[10px] bg-green-300"
+    var styling = "text-gray-800 font-ubuntu text-sm rounded-md w-[150px] h-[30px] mr-[10px] bg-green-300"
   } else {
-    var styling = "bg-gray-300 text-gray-800 font-ubuntu rounded-md w-[150px] h-[30px] mr-[10px] hover:bg-green-300"
+    var styling = "bg-gray-300 text-gray-800 text-sm font-ubuntu rounded-md w-[150px] h-[30px] mr-[10px] hover:bg-green-300"
   }
   return(
     <button 
@@ -160,58 +203,104 @@ const CategoryDropdown = (props)=> {
 }
 
 function App() {
-  //Load data from file
-
   const [activeView, setActiveView] = useState("All")
-  const [renderItems, setRenderItems] = useState([])
+  const [renderItems, setRenderItems] = useState(null)
   const [allNodes, setAllNodes] = useState(null)
-
+  const [options, setOptions] = useState(null)
+  const [name, setName] = useState("None Selected")
+  const [optionsError, setOptionsError] = useState(false)
+  const [errMsg, setErrMsg] = useState("")
+  const [profileError, setProfileError] = useState(false)
   useEffect(()=> {
 
-    const renderArrays = {
-      "User": [],
-      "Other": [],
-      "Node Modules": {},
-      "NodeJS Internal": {}
-    }
-    renderArrays["All"] = Object.keys(profile.node_map).map(key => {
-      return profile.node_map[key]
-    })
-    profile.categories.user.forEach((id, i) => {
-      renderArrays["User"].push(profile.node_map[id])
-    })
-    profile.categories.system.forEach((id, i) => {
-      renderArrays["Other"].push(profile.node_map[id])
-    })
-    
-    for (const [name, array] of Object.entries(profile.categories.npm_packages)) {
-      renderArrays["Node Modules"][name] = array.map((id) => {
-        return profile.node_map[id]
-      })
-    }
-    for (const [name, array] of Object.entries(profile.categories.node_js)) {
-      renderArrays["NodeJS Internal"][name] = array.map((id) => {
-        return profile.node_map[id]
-      })
-    }
-    setAllNodes(renderArrays)
-    setRenderItems(renderArrays["All"])
+    axios.get("http://localhost:8080/")
+          .then(res => {
+            const data = res.data
+            if (data.fail) {
+              setOptionsError(true)
+              setErrMsg(data.reason)
+            } else {
+              var opts = data.options.map((str) => {
+                return JSON.parse(str)
+              })
+              setOptions(opts)
+            }
+          })
+          .catch((err) => {
+            setOptionsError(true)
+            setErrMsg("Please ensure the Visualization server is running. Use the nodewatts --visualizer flag to run the server without profiling.")
+          })
   }, [])
 
+  const setupRenderObjects=(profile) => {
+      const renderArrays = {
+        "User": [],
+        "Other": [],
+        "Node Modules": {},
+        "NodeJS Internal": {}
+      }
+      renderArrays["All"] = Object.keys(profile.node_map).map(key => {
+        return profile.node_map[key]
+      })
+      profile.categories.user.forEach((id, i) => {
+        renderArrays["User"].push(profile.node_map[id])
+      })
+      profile.categories.system.forEach((id, i) => {
+        renderArrays["Other"].push(profile.node_map[id])
+      })
+      
+      for (const [name, array] of Object.entries(profile.categories.npm_packages)) {
+        renderArrays["Node Modules"][name] = array.map((id) => {
+          return profile.node_map[id]
+        })
+      }
+      for (const [name, array] of Object.entries(profile.categories.node_js)) {
+        renderArrays["NodeJS Internal"][name] = array.map((id) => {
+          return profile.node_map[id]
+        })
+      }
+      setAllNodes(renderArrays)
+      setRenderItems(renderArrays["All"])
+      setName(profile.name)
+      setActiveView("All")
+  }
+
   const viewHandler = (view) => {
-    setActiveView(view)
-    if (view === "Graph View"){
-      setRenderItems([])
-    } else {
-      setRenderItems(allNodes[view])
-    }
+      setActiveView(view)
+      if (view === "Graph View"){
+        setRenderItems([])
+      } else {
+        setRenderItems(allNodes[view])
+      }
+  }
+
+  const profileSwitchHandler = (id) => {
+      axios.get("http://localhost:8080/profiles", {params: {profile: id}})
+          .then(res => {
+            if (res.fail) {
+              setProfileError(true)
+              setErrMsg(res.reason)
+            } else {
+              const profile = JSON.parse(res.data.profile)
+              setupRenderObjects(profile)
+            }
+          })
+          .catch((err)=> {
+            console.log(err)
+            setProfileError(true)
+            setErrMsg("Unexpected request error. Check server logs")
+          })
+      
   }
 
   return (
     <div className="h-full pb-[50px] w-[1920px] m-auto pt-[20px] pl-[20px]" >
-      <Title name={profile.name}/>
-      <NavBar buttonHandler={viewHandler} activeView={activeView}></NavBar>
-      <ListView items={renderItems} categoryView={activeView === "NodeJS Internal" || activeView==="Node Modules"}/>
+      <Title name={name} handler={profileSwitchHandler} options={options}/>
+      {renderItems && <NavBar buttonHandler={viewHandler} activeView={activeView} />}
+      {renderItems && <ListView items={renderItems} categoryView={activeView === "NodeJS Internal" || activeView==="Node Modules"}/>}
+      {!renderItems && !optionsError && <span>Select a profile</span>}
+      {optionsError && <span>Failed to fetch saved profiles. {errMsg}</span>}
+      {!optionsError && profileError && <span>Failed to fetch the profile. {errMsg}</span>}
     </div>
   );
 }
